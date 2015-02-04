75e1de4e653d6850368bcaeaa48add309802d7cc
Adam Griffin <adam.griffin@moxiworks.com>
2015-02-03 17:21:40 -0800

    Added prelimiary code to delete columns

--- cms_exchanger.rb
+++ cms_exchanger.rb
@@ -8,9 +8,45 @@ require 'active_support/core_ext/hash/slice'
 # "E-mail Address" would need to be changed to "E-mail" though 
 unique_to_o365 = ["Business street", "Home street", "E-mail", "Web page"]
 
-unique_to_cms = ["Business Street",
- "Business Street 3",
- "Home Street",
+# unique_to_cms = ["Business Street",
+#  "Business Street 3",
+#  "Home Street",
+#  "Home Street 3",
+#  "Other Street 3",
+#  "Car Phone",
+#  "ISDN",
+#  "Account",
+#  "Anniversary",
+#  "Billing Information",
+#  "Birthday",
+#  "Directory Server",
+#  "E-mail Address",
+#  "Gender",
+#  "Government ID Number",
+#  "Hobby",
+#  "Initials",
+#  "Internet Free Busy",
+#  "Keywords",
+#  "Language",
+#  "Location",
+#  "Mileage",
+#  "Organizational ID Number",
+#  "Priority",
+#  "Private",
+#  "Profession",
+#  "Referred By",
+#  "Sensitivity",
+#  "Spouse",
+#  "User 1",
+#  "User 2",
+#  "User 3",
+#  "User 4",
+#  "Web Page"]
+
+# test this out without changing "Business street", "Home street", or "Web page"
+# will need to change "E-mail Address" to "Email" once done running
+# test to see if the case matters for those columns (it shouldn't)
+unique_to_cms = ["Business Street 3",
  "Home Street 3",
  "Other Street 3",
  "Car Phone",
@@ -20,7 +56,6 @@ unique_to_cms = ["Business Street",
  "Billing Information",
  "Birthday",
  "Directory Server",
- "E-mail Address",
  "Gender",
  "Government ID Number",
  "Hobby",
@@ -40,5 +75,37 @@ unique_to_cms = ["Business Street",
  "User 1",
  "User 2",
  "User 3",
- "User 4",
- "Web Page"]
+ "User 4"]
+
+# configure command line options
+# more will go here eventually
+options = {}
+optparse = OptionParser.new do|opts|
+  opts.banner = "cms_exchanger: translate CMS export files to Office 365 format\n
+  Usage: top_exchanger inputfile [outputfile]"
+  opts.on( '-h', '--help', 'Display this screen' ) do
+     puts opts
+     exit
+  end
+end
+optparse.parse!
+# Check if files were specified
+if ARGV.empty?
+  puts optparse
+  exit(-1)
+end
+export_file = ARGV.shift # this is the the export file we're going to translate
+import_file = ARGV.shift # this is the translated file we're going to ouput
+import_file = "O365_import_file.csv" if !import_file
+
+# get headers from the import template
+import_headers = CSV.open("O365_import_template.csv").first
+# open output file & add headers
+output = CSV.open(import_file, "wb")
+output << import_headers
+
+CSV.foreach(export_file, headers: true) do |contact|
+  # new_contact = CSV::Row.new(import_headers, [])
+  contact.delete_if {|header, field| unique_to_cms.include? header}
+  output << contact
+end
