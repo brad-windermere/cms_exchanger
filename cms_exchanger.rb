3a571b7aa32f921722bdf1e4d78d170d976cade4
Adam Griffin <adam@agriffin.me>
2015-02-08 23:39:45 -0800

    Did a major re-write. The tool now opens the export csv as a normal file and fixes problems with the data before trying to parse as csv. It is working with the full export instead of the outlook export csv now, becaue the full export has all quoted fields. This helped to get around bugs where there was a comma within the text of a field that was not escaped. TODO: contactnate some address fields.

--- cms_exchanger.rb
+++ cms_exchanger.rb
@@ -23,7 +23,7 @@ if ARGV.empty?
 end
 export_file = ARGV.shift # this is the the export file we're going to translate
 import_file = ARGV.shift # this is the translated file we're going to ouput
-import_file = "O365_import_file.csv" if !import_file
+import_file = "O365_import_file.csv" if !import_file # should use =|| here instead
 
 # get headers from the import template
 import_headers = CSV.open("O365_import_template.csv").first
@@ -32,12 +32,24 @@ output = CSV.open(import_file, "wb")
 output << import_headers
 
 # this csv has mappings between cms -> exhange fields
-mapping = CSV.open('field_mapping.csv', headers: true).read
+# mapping = CSV.open('field_mapping.csv', headers: true).read
+mapping = CSV.open('field_mapping-full_export.csv', headers: true).read
 
-CSV.foreach(export_file, encoding: "ISO-8859-1:UTF-8", headers: true) do |contact|
+export = File.open(export_file, encoding: "ISO-8859-1:UTF-8")
+export_headers = export.readline("\r").chomp.split(',') # have to specify \r since it is not default line ending
+export.each_line("\r") do |line|
+  # not only is there a \r to get rid of, but there is an extra comma for some reason!
+  # regex escapes "" that are nested within quoted fields like so: ""nested""
+  fixed = line.chomp.chop.gsub(/(?<!^|,)"(?!,|$)/,'""')
+  next if fixed.empty? # these crappy files have empty lines for some reason
+  contact = CSV::Row.new( export_headers, CSV.parse_line(fixed) )
   new_contact = CSV::Row.new(import_headers, [])
   mapping.each do |column|
-    new_contact[ column["Exchange Column"] ] = "#{ contact[ column['CMS DB Column'] ] }".strip # get rid of fields with nothing but blank space
+    new_contact[ column["Exchange Column"] ] = contact[ column['CMS DB Column'] ].to_s.strip # get rid of fields with nothing but blank space
   end
   output << new_contact
 end
+
+
+
+
