386826d76737129ffeb6c3d865d51b71c969b1a4
Adam Griffin <adam@agriffin.me>
2015-02-10 22:37:08 -0800

    Added all exchange fields to mapping.yml. This way there is no need for a separate file for the import headers. Changed code so that exchange import headers are now derived from mapping.yml keys.

--- cms_exchanger.rb
+++ cms_exchanger.rb
@@ -8,8 +8,10 @@ require 'pry'
 
 NEWLINE = "\r"
 mapping = YAML.load_file("mapping.yml")
+# get headers for exchange import file
+IMPORT_HEADERS = mapping.keys
 
-def error(usage, message=nil)
+def error(usage, message)
   puts message if message
   puts usage
   exit(-1) 
@@ -40,51 +42,40 @@ optparse = OptionParser.new do|opts|
   end
 end
 optparse.parse!
-# Check if files were specified
-if ARGV.empty?
-  error(optparse, nil)
-end
+# check if files were specified, handle errors
+error(optparse, nil) if ARGV.empty?
 export_file = ARGV.shift # this is the the export file we're going to translate
-unless File.file?(export_file)
-  error(optparse, "#{export_file} doesn't exist")
-end
-import_file = ARGV.shift || "O365_import_file.csv" # this is the translated file we're going to ouput
+error(optparse, "#{export_file} doesn't exist") unless File.file?(export_file)
 
-# get headers from the import template
-import_headers = CSV.open("O365_import_template.csv").first
-# open output file & add headers
-output = CSV.open(import_file, "wb")
-output << import_headers
+# this is the translated file we're going to ouput, give it a name if not specified
+import_file = ARGV.shift || "O365_import_file.csv"
+output = CSV.open(import_file, "w")
+output << IMPORT_HEADERS
 
 # try to guess text encoding. it's usually iso-8859-1 or us-ascii
 encoding = `file -b --mime-encoding #{export_file}`.chomp
+# this has to be opened as a plain file instead of csv because it may
+# have unescaped quotes that need to be dealt with before it can be parsed
 export = File.open(export_file, encoding: "#{encoding}:UTF-8")
-export_headers = export.readline(NEWLINE).chomp.split(',') # have to specify newline char since it is not \n
+EXPORT_HEADERS = export.readline(NEWLINE).chomp.split(',') # have to specify newline char since it is not \n
+
 export.each_line(NEWLINE) do |line|
-  # not only is there a \r to get rid of, but there is an extra comma for some reason!
-  # regex escapes "" that are nested within quoted fields like so: ""nested""
+  # not only is there a \r to get rid of, but there is an extra comma for some reason! hend chop.comp()
+  # regex escapes "" that are nested within quoted fields so they are double instead like: ""nested""
   escaped = line.chomp.chop.gsub(/(?<!^|,)"(?!,|$)/,'""')
-  next if escaped.empty? # these crappy files have empty lines for some reason
-  contact = CSV::Row.new( export_headers, CSV.parse_line(escaped) )
-  new_contact = CSV::Row.new(import_headers, [])
+  next if escaped.empty? # these csv files have empty lines at the end sometimes
+  contact = CSV::Row.new( EXPORT_HEADERS, CSV.parse_line(escaped) )
+  new_contact = CSV::Row.new( IMPORT_HEADERS, [] )
 
   mapping.each do |exchange_col, cms_col|
     if cms_col.kind_of?(Hash)
-      # binding.pry
       cms_col.each do |method, fields|
         new_contact[exchange_col] = concat_fields( method, contact.to_hash.slice(*fields).values )
       end
     else
-      new_contact[exchange_col] = contact[cms_col].to_s.strip # strip fields with nothing but blank space
+      new_contact[exchange_col] = contact[cms_col].to_s.strip # strip the fields with nothing but blank space
     end
   end
 
-  # mapping.each do |column|
-  #   new_contact[ column["Exchange Column"] ] = contact[ column['CMS DB Column'] ].to_s.strip # strip fields with nothing but blank space
-  # end
   output << new_contact
 end
-
-
-
-
